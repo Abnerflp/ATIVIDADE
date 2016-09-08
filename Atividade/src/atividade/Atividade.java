c5142f211af7e710d789efbac041e53abb28a5e5
Aluno <Aluno@172.18.0.188>
2016-09-08 14:18:43 -0300

    segundoTESTE

--- Atividade/src/atividade/Atividade.java
+++ Atividade/src/atividade/Atividade.java
@@ -16,6 +16,7 @@ public class Atividade {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("primeiro teste");
     }
     
 }
